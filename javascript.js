69d5458351a7cf46ad732fb94fffebb47574bec3
cowlesrk <cowlesrk@yahoo.com>
2017-05-16 15:14:46 -0400

    created a button that will toggle between showing and hiding the list

--- javascript.js
+++ javascript.js
@@ -165,8 +165,88 @@
 
 
 
-// Changing the list description with user input
+// Changing text attribute to a checkbox
 // ------------------------------------------------------------------------
+
+// const input = document.querySelector('input');
+// input.type = "checkbox";
+
+
+
+// Changing styles of the paragraph element
+// ------------------------------------------------------------------------
+
+// const p = document.querySelector("p.description");
+
+// p.style.color = "darkblue";
+// p.style.backgroundColor = "lightblue";
+
+
+
+// Hide / Show list toggle button
+// ------------------------------------------------------------------------
+
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector(".list");
+const input = document.querySelector("input.description");
+const p = document.querySelector("p.description");
+const button = document.querySelector("button.description");
+
+toggleList.addEventListener("click", () => {
+	if (listDiv.style.display == "none") {
+		toggleList.textContent = "Hide List";
+		listDiv.style.display = "block";
+	} else {
+		toggleList.textContent = "Show List";
+		listDiv.style.display = "none";
+	}
+});
+
+button.addEventListener("click", () => {
+	p.textContent = input.value + ":";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
