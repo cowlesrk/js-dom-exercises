daf6a8c83acc4d83eacbf8dd549fc96503ec772e
cowlesrk <cowlesrk@yahoo.com>
2017-05-19 16:41:31 -0400

    added function to generate buttons

--- javascript.js
+++ javascript.js
@@ -560,6 +560,8 @@
 // });
 
 
+
+
 // A delegated click event listener has been attached to the selected ul element, 
 // which is stored in the variable list. The handler is targeting each button in the list. 
 // When any one of the buttons is clicked, a class of highlight should be added to 
@@ -577,6 +579,94 @@
 
 // });
 
+
+
+// Getting all children of a node with children
+// Move list item down when Down button is clicked
+// ------------------------------------------------------------------------
+
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector(".list");
+const descriptionInput = document.querySelector("input.description");
+const decriptionP = document.querySelector("p.description");
+const descriptionButton = document.querySelector("button.description");
+const listUl = listDiv.querySelector("ul");
+const addItemInput = document.querySelector("input.addItemInput");
+const addItemButton = document.querySelector("button.addItemButton");
+const lis = listUl.children;
+
+function attachListItemButtons(li) {
+	let up = document.createElement("button");
+	up.className = "up";
+	up.textContent = "Up";
+	li.appendChild(up);
+
+	let down = document.createElement("button");
+	down.className = "down";
+	down.textContent = "Down";
+	li.appendChild(down);
+
+	let remove = document.createElement("button");
+	remove.className = "remove";
+	remove.textContent = "Remove";
+	li.appendChild(remove);
+}
+
+for (let i = 0; i < lis.length; i += 1){
+	attachListItemButtons(lis[i]);
+}
+
+listUl.addEventListener("click", (event) => {
+	if (event.target.tagName == "BUTTON") {
+		if (event.target.className == "remove") {
+			let li = event.target.parentNode;
+			let ul = li.parentNode;
+			ul.removeChild(li);
+		}
+		if (event.target.className == "up") {
+			let li = event.target.parentNode;
+			let prevLi = li.previousElementSibling;
+			let ul = li.parentNode;
+			if (prevLi) {
+			ul.insertBefore(li, prevLi);
+			}
+			
+		}
+		if (event.target.className == "down") {
+			let li = event.target.parentNode;
+			let nextLi = li.nextElementSibling;
+			let ul = li.parentNode;
+			if (nextLi) {
+			ul.insertBefore(nextLi, li);
+			}
+			
+		}
+	}
+});
+
+toggleList.addEventListener("click", () => {
+	if (listDiv.style.display == "none") {
+		toggleList.textContent = "Hide List";
+		listDiv.style.display = "block";
+	} else {
+		toggleList.textContent = "Show List";
+		listDiv.style.display = "none";
+	}
+});
+
+descriptionButton.addEventListener("click", () => {
+	decriptionP.innerHTML = descriptionInput.value + ":";
+	descriptionInput.value = "";
+});
+
+addItemButton.addEventListener("click", () => {
+	let ul = document.getElementsByTagName("ul")[0];
+	let li = document.createElement("li");
+	li.textContent = addItemInput.value;
+	attachListItemButtons(li);
+	ul.appendChild(li);
+	addItemInput.value = "";
+});
 
 
 
