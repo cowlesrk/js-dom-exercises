122ed449f6355aa6e9a13e31e2e5bb73d11369a3
cowlesrk <cowlesrk@yahoo.com>
2017-05-19 16:30:48 -0400

    added challenge and challenge solution

--- javascript.js
+++ javascript.js
@@ -499,69 +499,83 @@
 // Move list item down when Down button is clicked
 // ------------------------------------------------------------------------
 
-const toggleList = document.getElementById('toggleList');
-const listDiv = document.querySelector(".list");
-const descriptionInput = document.querySelector("input.description");
-const decriptionP = document.querySelector("p.description");
-const descriptionButton = document.querySelector("button.description");
-const listUl = listDiv.querySelector("ul");
-const addItemInput = document.querySelector("input.addItemInput");
-const addItemButton = document.querySelector("button.addItemButton");
+// const toggleList = document.getElementById('toggleList');
+// const listDiv = document.querySelector(".list");
+// const descriptionInput = document.querySelector("input.description");
+// const decriptionP = document.querySelector("p.description");
+// const descriptionButton = document.querySelector("button.description");
+// const listUl = listDiv.querySelector("ul");
+// const addItemInput = document.querySelector("input.addItemInput");
+// const addItemButton = document.querySelector("button.addItemButton");
 
-listUl.addEventListener("click", (event) => {
-	if (event.target.tagName == "BUTTON") {
-		if (event.target.className == "remove") {
-			let li = event.target.parentNode;
-			let ul = li.parentNode;
-			ul.removeChild(li);
-		}
-		if (event.target.className == "up") {
-			let li = event.target.parentNode;
-			let prevLi = li.previousElementSibling;
-			let ul = li.parentNode;
-			if (prevLi) {
-			ul.insertBefore(li, prevLi);
-			}
+// listUl.addEventListener("click", (event) => {
+// 	if (event.target.tagName == "BUTTON") {
+// 		if (event.target.className == "remove") {
+// 			let li = event.target.parentNode;
+// 			let ul = li.parentNode;
+// 			ul.removeChild(li);
+// 		}
+// 		if (event.target.className == "up") {
+// 			let li = event.target.parentNode;
+// 			let prevLi = li.previousElementSibling;
+// 			let ul = li.parentNode;
+// 			if (prevLi) {
+// 			ul.insertBefore(li, prevLi);
+// 			}
 			
-		}
-		if (event.target.className == "down") {
-			let li = event.target.parentNode;
-			let nextLi = li.nextElementSibling;
-			let ul = li.parentNode;
-			if (nextLi) {
-			ul.insertBefore(nextLi, li);
-			}
+// 		}
+// 		if (event.target.className == "down") {
+// 			let li = event.target.parentNode;
+// 			let nextLi = li.nextElementSibling;
+// 			let ul = li.parentNode;
+// 			if (nextLi) {
+// 			ul.insertBefore(nextLi, li);
+// 			}
 			
-		}
-	}
-});
+// 		}
+// 	}
+// });
 
-toggleList.addEventListener("click", () => {
-	if (listDiv.style.display == "none") {
-		toggleList.textContent = "Hide List";
-		listDiv.style.display = "block";
-	} else {
-		toggleList.textContent = "Show List";
-		listDiv.style.display = "none";
-	}
-});
+// toggleList.addEventListener("click", () => {
+// 	if (listDiv.style.display == "none") {
+// 		toggleList.textContent = "Hide List";
+// 		listDiv.style.display = "block";
+// 	} else {
+// 		toggleList.textContent = "Show List";
+// 		listDiv.style.display = "none";
+// 	}
+// });
 
-descriptionButton.addEventListener("click", () => {
-	decriptionP.innerHTML = descriptionInput.value + ":";
-	descriptionInput.value = "";
-});
+// descriptionButton.addEventListener("click", () => {
+// 	decriptionP.innerHTML = descriptionInput.value + ":";
+// 	descriptionInput.value = "";
+// });
 
-addItemButton.addEventListener("click", () => {
-	let ul = document.getElementsByTagName("ul")[0];
-	let li = document.createElement("li");
-	li.textContent = addItemInput.value;
-	ul.appendChild(li);
-	addItemInput.value = "";
-});
-
-
+// addItemButton.addEventListener("click", () => {
+// 	let ul = document.getElementsByTagName("ul")[0];
+// 	let li = document.createElement("li");
+// 	li.textContent = addItemInput.value;
+// 	ul.appendChild(li);
+// 	addItemInput.value = "";
+// });
 
 
+// A delegated click event listener has been attached to the selected ul element, 
+// which is stored in the variable list. The handler is targeting each button in the list. 
+// When any one of the buttons is clicked, a class of highlight should be added to 
+// the paragraph element immediately preceding that button inside the parent list item element. 
+// Add the code to create this behavior on line 5.
+// ------------------------------------------------------------------------
+
+// const list = document.getElementsByTagName('ul')[0];
+
+// list.addEventListener('click', function(e) {
+
+//   if (e.target.tagName == 'BUTTON') {
+// 	let p = e.target.previousElementSibling;
+// 	p.className = 'highlight';  }
+
+// });
 
 
 
