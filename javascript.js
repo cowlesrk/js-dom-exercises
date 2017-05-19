bebfd9cad2c51402b5e6c4079e05a10e471287da
cowlesrk <cowlesrk@yahoo.com>
2017-05-19 16:01:51 -0400

    remove list item when clicking a button (using parentNode to traverse up the dom)

--- javascript.js
+++ javascript.js
@@ -390,28 +390,82 @@
 // Added event listener to parent element and let it handle events on its children
 // ------------------------------------------------------------------------
 
+// const toggleList = document.getElementById('toggleList');
+// const listDiv = document.querySelector(".list");
+// const descriptionInput = document.querySelector("input.description");
+// const decriptionP = document.querySelector("p.description");
+// const descriptionButton = document.querySelector("button.description");
+// const addItemInput = document.querySelector("input.addItemInput");
+// const addItemButton = document.querySelector("button.addItemButton");
+// const removeItemButton = document.querySelector("button.removeItemButton");
+
+
+// listDiv.addEventListener("mouseover", (event) => {
+// 	if (event.target.tagName == "LI") {
+// 		event.target.textContent = event.target.textContent.toUpperCase();
+// 	}
+// });
+
+// listDiv.addEventListener("mouseout", (event) => {
+// 	if (event.target.tagName == "LI") {
+// 		event.target.textContent = event.target.textContent.toLowerCase();
+// 	}
+// });
+
+
+// toggleList.addEventListener("click", () => {
+// 	if (listDiv.style.display == "none") {
+// 		toggleList.textContent = "Hide List";
+// 		listDiv.style.display = "block";
+// 	} else {
+// 		toggleList.textContent = "Show List";
+// 		listDiv.style.display = "none";
+// 	}
+// });
+
+
+// descriptionButton.addEventListener("click", () => {
+// 	decriptionP.innerHTML = descriptionInput.value + ":";
+// 	descriptionInput.value = "";
+// });
+
+
+// addItemButton.addEventListener("click", () => {
+// 	let ul = document.getElementsByTagName("ul")[0];
+// 	let li = document.createElement("li");
+// 	li.textContent = addItemInput.value;
+// 	ul.appendChild(li);
+// 	addItemInput.value = "";
+// });
+
+// removeItemButton.addEventListener("click", () => {
+// 	let ul = document.getElementsByTagName("ul")[0];
+// 	let li = document.querySelector("li:last-child");
+// 	ul.removeChild(li);
+// });
+
+
+
+// Using parentNode to Traverse Up the DOM
+// Remove list item when clicking a button
+// ------------------------------------------------------------------------
+
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector(".list");
 const descriptionInput = document.querySelector("input.description");
 const decriptionP = document.querySelector("p.description");
 const descriptionButton = document.querySelector("button.description");
+const listUl = listDiv.querySelector("ul");
 const addItemInput = document.querySelector("input.addItemInput");
 const addItemButton = document.querySelector("button.addItemButton");
-const removeItemButton = document.querySelector("button.removeItemButton");
 
-
-listDiv.addEventListener("mouseover", (event) => {
-	if (event.target.tagName == "LI") {
-		event.target.textContent = event.target.textContent.toUpperCase();
+listUl.addEventListener("click", (event) => {
+	if (event.target.tagName == "BUTTON") {
+		let li = event.target.parentNode;
+		let ul = li.parentNode;
+		ul.removeChild(li);
 	}
 });
-
-listDiv.addEventListener("mouseout", (event) => {
-	if (event.target.tagName == "LI") {
-		event.target.textContent = event.target.textContent.toLowerCase();
-	}
-});
-
 
 toggleList.addEventListener("click", () => {
 	if (listDiv.style.display == "none") {
@@ -423,12 +477,10 @@ toggleList.addEventListener("click", () => {
 	}
 });
 
-
 descriptionButton.addEventListener("click", () => {
 	decriptionP.innerHTML = descriptionInput.value + ":";
 	descriptionInput.value = "";
 });
-
 
 addItemButton.addEventListener("click", () => {
 	let ul = document.getElementsByTagName("ul")[0];
@@ -437,14 +489,6 @@ addItemButton.addEventListener("click", () => {
 	ul.appendChild(li);
 	addItemInput.value = "";
 });
-
-removeItemButton.addEventListener("click", () => {
-	let ul = document.getElementsByTagName("ul")[0];
-	let li = document.querySelector("li:last-child");
-	ul.removeChild(li);
-});
-
-
 
 
 
