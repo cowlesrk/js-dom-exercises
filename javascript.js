8d470816da32fc69a0b357ccbb5ec2f90f5f2858
cowlesrk <cowlesrk@yahoo.com>
2017-05-19 14:41:22 -0400

    capitalize list item when you mouse over, convert back to lowercase when you mouse out

--- javascript.js
+++ javascript.js
@@ -323,13 +323,66 @@
 // Functions as Parameters
 // ------------------------------------------------------------------------
 
- window.setTimeout((something) => {
- 	console.log(something);
- }, 3000, "Greetings, everyone!");
+ // window.setTimeout((something) => {
+ // 	console.log(something);
+ // }, 3000, "Greetings, everyone!");
 
 
 
+// Listening for Events with addEventListener()
+// Capitalizing an item when you mouse over
+// ------------------------------------------------------------------------
 
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector(".list");
+const descriptionInput = document.querySelector("input.description");
+const decriptionP = document.querySelector("p.description");
+const descriptionButton = document.querySelector("button.description");
+const addItemInput = document.querySelector("input.addItemInput");
+const addItemButton = document.querySelector("button.addItemButton");
+const removeItemButton = document.querySelector("button.removeItemButton");
+const listItems = document.getElementsByTagName("li");
+
+for (let i = 0; i < listItems.length; i += 1) {
+	listItems[i].addEventListener("mouseover", () => {
+		listItems[i].textContent = listItems[i].textContent.toUpperCase();
+	});
+
+	listItems[i].addEventListener("mouseout", () => {
+		listItems[i].textContent = listItems[i].textContent.toLowerCase();
+	});
+};
+
+toggleList.addEventListener("click", () => {
+	if (listDiv.style.display == "none") {
+		toggleList.textContent = "Hide List";
+		listDiv.style.display = "block";
+	} else {
+		toggleList.textContent = "Show List";
+		listDiv.style.display = "none";
+	}
+});
+
+
+descriptionButton.addEventListener("click", () => {
+	decriptionP.innerHTML = descriptionInput.value + ":";
+	descriptionInput.value = "";
+});
+
+
+addItemButton.addEventListener("click", () => {
+	let ul = document.getElementsByTagName("ul")[0];
+	let li = document.createElement("li");
+	li.textContent = addItemInput.value;
+	ul.appendChild(li);
+	addItemInput.value = "";
+});
+
+removeItemButton.addEventListener("click", () => {
+	let ul = document.getElementsByTagName("ul")[0];
+	let li = document.querySelector("li:last-child");
+	ul.removeChild(li);
+});
 
 
 
